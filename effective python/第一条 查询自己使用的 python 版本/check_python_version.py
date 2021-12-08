d21fbfb08d1bba751e52bd81dc7e17c0b57694c3
pengenlai <1848974065@qq.com>
2021-12-08 20:40:54 +0800

    effective python

---
+++ effective python/第一条 查询自己使用的 python 版本/check_python_version.py
@@ -0,0 +1,10 @@
+import sys
+
+# 打印出 python 所属的版本
+print(sys.version)
+
+'''
+python3 是最新版的 python， 而且受到了很大的支持，大家应该用 python3 开发项目
+在操作系统的命令行界面运行 python 时，要确认该 python 的版本是否跟你要使用的版本相同
+不要再使用 python2 做开发了, 因为该版本已于2020年1月1日停止更新维护
+'''
